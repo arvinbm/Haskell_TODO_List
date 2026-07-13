e947a35f44642dd96511b8fc1ea339efa323cb51
Arvin Bayat Manesh <aba191@sfu.ca>
2026-07-13 16:58:14 -0700

    Add addTask & renderAll

--- todo.hs
+++ todo.hs
@@ -7,3 +7,10 @@ render :: Task -> String
 render t = if done t
     then "[x] " ++ description t
     else "[ ] " ++ description t
+
+addTask :: String -> [Task] -> [Task]
+addTask desc taskList = Task { description = desc, done = False } : taskList
+
+renderAll :: [Task] -> String
+renderAll taskList = unlines (map render taskList)
+
