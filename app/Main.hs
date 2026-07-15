04bc41540ebd29bf3061dbbb727ddb4aaad51c09
Arvin Bayat Manesh <aba191@sfu.ca>
2026-07-15 01:12:29 -0700

    tidy CLI

--- app/Main.hs
+++ app/Main.hs
@@ -72,6 +72,23 @@ loadTasks = do
 saveTasks :: [Task] -> IO ()
 saveTasks taskList = writeFile "tasks.txt" (show taskList)
 
+-- Does any task in the list have this description?
+taskExists :: String -> [Task] -> Bool
+taskExists desc taskList = any (\task -> description task == desc) taskList
+
+-- The command overview, shown at start and after an unknown command.
+help :: String
+help = unlines
+    [ "Commands:"
+    , "  add <task>       add a new task"
+    , "  done <number>    mark a task as done (done <task> works too)"
+    , "  remove <task>    delete a task"
+    , "  list             show all tasks"
+    , "  count            show how many tasks are left to do"
+    , "  clear            delete all completed tasks"
+    , "  quit             save and exit"
+    ]
+
 -- The heart of the app: read a command, act on it, then loop
 -- again with the updated list. The list is never modified --
 -- each round passes a new list to the next round.
@@ -81,37 +98,70 @@ loop taskList = do
     case words line of
         ["quit"] -> do
             saveTasks taskList
-            putStrLn "Bye!"
+            putStrLn "Saved. Bye!"
 
         ["list"] -> do
-            putStrLn (renderAll taskList)
+            if null taskList
+                then putStrLn "Nothing to do!"
+                else putStr (renderAll taskList)
             loop taskList
 
         ["count"] -> do
-            putStrLn (show (countUndone taskList))
+            putStrLn (show (countUndone taskList) ++ " task(s) left to do")
             loop taskList
 
-        ["clear"] -> loop (clearTasks taskList)
+        ["clear"] -> do
+            let newList = clearTasks taskList
+            putStrLn ("Cleared " ++ show (length taskList - length newList) ++ " completed task(s)")
+            loop newList
 
-        ("add":rest) -> loop (addTask (unwords rest) taskList)
+        ("add":rest) -> do
+            let desc = unwords rest
+            if null desc
+                then do
+                    putStrLn "Nothing to add. Try: add buy milk"
+                    loop taskList
+                else do
+                    putStrLn ("Added: " ++ desc)
+                    loop (addTask desc taskList)
 
-        ("remove":rest) -> loop (removeTask (unwords rest) taskList)
+        ("remove":rest) -> do
+            let desc = unwords rest
+            if taskExists desc taskList
+                then do
+                    putStrLn ("Removed: " ++ desc)
+                    loop (removeTask desc taskList)
+                else do
+                    putStrLn ("No task called '" ++ desc ++ "'")
+                    loop taskList
 
         ("done":rest) ->
             case readMaybe (unwords rest) of
                 Just n -> if n >= 1 && n <= length taskList
-                    then loop (completeTaskAt n taskList)
-                    else do 
+                    then do
+                        -- !! picks an item by position, counting from 0
+                        putStrLn ("Done: " ++ description (taskList !! (n - 1)))
+                        loop (completeTaskAt n taskList)
+                    else do
                         putStrLn ("Task " ++ show n ++ " does not exist")
                         loop taskList
-                Nothing -> loop (completeTask (unwords rest) taskList)
+                Nothing -> do
+                    let desc = unwords rest
+                    if taskExists desc taskList
+                        then do
+                            putStrLn ("Done: " ++ desc)
+                            loop (completeTask desc taskList)
+                        else do
+                            putStrLn ("No task called '" ++ desc ++ "'")
+                            loop taskList
 
         _ -> do
-            putStrLn "Commands: add <task>, done <task>, done <task number>, remove <task>, clear, count, list, quit"
+            putStr help
             loop taskList
 
 main :: IO ()
 main = do
-    putStrLn "Commands: add <task>, done <task>, done <task number>, remove <task>, clear, list, count, quit"
+    putStrLn "Your TODO list."
+    putStr help
     taskList <- loadTasks
     loop taskList
